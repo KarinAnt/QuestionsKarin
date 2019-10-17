ae31f88487bf157e0afe351b59cb11a78dda3e77
KarinAnt <antonyankarinka@gmail.com>
2019-10-17 18:51:27 +0400

    Question js

--- js.js
+++ js.js
@@ -1,17 +1,16 @@
-const questionsandansvers =[{"question":"Which is the world's highest mountain?", 0:"K2", 1:"Kilimanjaro", 2:"Mount Everest", 3:"Makalu"},
-{"question":"Which of these cities is not in Europe?", 0:"Barcelona", 1:"Prague", 2:"Moscow", 3:"Reykjavik"},
-{"question":"Which of the following countries do not border France?", 0:"Netherlands", 1:"Germany", 2:"Spain", 3:"Italy"},
-{"question":"What is the approximate size of Earth's equator?" , 0:"40.000 km", 1:"20.000 km", 2:"30.000 km", 3:"50.000 km"},
-{"question":"Which is the smallest country, measured by total land area?", 0:"Monaco", 1:"Vatican City", 2:"Maldives", 3:"Tuvalu"}
+const questionsAndAnsvers =[{"question":"Which is the world's highest mountain?", 0:"K2", 1:"Kilimanjaro", 2:"Mount Everest", 3:"Makalu", "answer":2},
+{"question":"Which of these cities is not in Europe?", 0:"Barcelona", 1:"Prague", 2:"Moscow", 3:"Reykjavik", "answer":2},
+{"question":"Which of the following countries do not border France?", 0:"Netherlands", 1:"Germany", 2:"Spain", 3:"Italy", "answer":0},
+{"question":"What is the approximate size of Earth's equator?" , 0:"40.000 km", 1:"20.000 km", 2:"30.000 km", 3:"50.000 km", "answer":0},
+{"question":"Which is the smallest country, measured by total land area?", 0:"Monaco", 1:"Vatican City", 2:"Maldives", 3:"Tuvalu", "answer":1}
 ];
 var checkedarray = [0,0,0,0,0];
-var truearray = [2,2,0,0,1];
 var countback = 0;
 var checkedi;
 var haveback = 0;
 var startbutton = document.getElementById("startbutton");
+var usernameinput = document.getElementById("usernameinput");
 var username = document.getElementById("username");
-var pusername = document.getElementById("pusername");
 var questions = document.getElementById('questions');
 var idn = document.getElementById('idn');
 var question = document.getElementById('question');
@@ -23,27 +22,28 @@ var next = document.getElementById('next');
 var result = document.getElementById('result');
 var newgame = document.getElementById('newgame');
 var answersspan = document.getElementsByClassName('answersspan');
-var divs = document.getElementsByClassName('divs');
 var radio = document.getElementsByName('one');
 startbutton.addEventListener('click', function(){
-    if(username.value){
+    if(usernameinput.value){
         start.style.display = "none";
         next.style.display = "inline-block";
         back.style.display = "none";
         end.style.display = "none";
         questions.style.display = "flex"; 
-        pusername.innerHTML = username.value;
-        question.innerHTML = "." + questionsandansvers[0].question;
+        username.innerHTML = usernameinput.value;
+        question.innerHTML = "." + questionsAndAnsvers[0].question;
         idn.innerHTML = 1;
-        for(let i = 0; i<answersspan.length; i++){
-            answersspan[i].innerHTML = questionsandansvers[0][i];
+        for(let i = 0; i < answersspan.length; i++){
+            answersspan[i].innerHTML = questionsAndAnsvers[0][i];
         }
     }
 })
 next.addEventListener("click", function(){
-    if(haveback) countback = 0;
+    if(haveback){
+        countback = 0;
+    } 
     let a = 0;
-    for(let i = 0; i< radio.length; i++){
+    for(let i = 0; i < radio.length; i++){
         if(radio[i].checked === true){
             a = 1;
             checkedi = i;
@@ -54,16 +54,16 @@ next.addEventListener("click", function(){
         return;
     }
     let b = idn.innerHTML;
-    if(idn.innerHTML == questionsandansvers.length-1){
+    if(idn.innerHTML == questionsAndAnsvers.length-1){
         next.style.display = "none";
         end.style.display = "inline-block";
     }
-    question.innerHTML = "." + questionsandansvers[b].question;
+    question.innerHTML = "." + questionsAndAnsvers[b].question;
     idn.innerHTML = +b + 1;
-    for(let i = 0; i<answersspan.length; i++){
-        answersspan[i].innerHTML = questionsandansvers[b][i];
+    for(let i = 0; i < answersspan.length; i++){
+        answersspan[i].innerHTML = questionsAndAnsvers[b][i];
     }
-    for(let i = 0; i< radio.length; i++){
+    for(let i = 0; i < radio.length; i++){
         radio[i].checked = false;  
     }
     if(idn.innerHTML != 1){
@@ -80,12 +80,12 @@ back.addEventListener('click', function(){
         back.style.display = "none";        
     }
     let b = idn.innerHTML;
-    question.innerHTML = "." + questionsandansvers[b-2].question;
+    question.innerHTML = "." + questionsAndAnsvers[b-2].question;
     idn.innerHTML = +b - 1;
-    for(let i = 0; i<answersspan.length; i++){
-        answersspan[i].innerHTML = questionsandansvers[b-2][i];
+    for(let i = 0; i < answersspan.length; i++){
+        answersspan[i].innerHTML = questionsAndAnsvers[b-2][i];
     }
-    if(idn.innerHTML != questionsandansvers.length-2){
+    if(idn.innerHTML != questionsAndAnsvers.length-2){
         next.style.display = "inline-block";
         end.style.display = "none";
     }
@@ -98,7 +98,7 @@ for(let i = 0; i < radio.length; i++){
 }
 end.addEventListener("click", function(){
     let a = 0;
-    for(let i = 0; i< radio.length; i++){
+    for(let i = 0; i < radio.length; i++){
         if(radio[i].checked === true){
             a = 1;
             break;
@@ -112,42 +112,42 @@ end.addEventListener("click", function(){
     questions.style.display = "none";
     let points = 0;
     checkedarray.forEach(function(v,i){
-        if(v == truearray[i]){
+        if(v == questionsAndAnsvers[i].answer){
             points ++;
         }
     })
-    nameandpoints.innerHTML = pusername.innerHTML + " - " + points +"/5";
-    for(let i = 0; i < questionsandansvers.length; i++){
-        var divs = document.createElement('div');
-        var pq = document.createElement("p");
-        var pt = document.createElement("p");
+    nameandpoints.innerHTML = username.innerHTML + " - " + points +"/5";
+    for(let i = 0; i < questionsAndAnsvers.length; i++){
+        var endQuestionsAndAnsvers = document.createElement('div');
+        var endQuestion = document.createElement("p");
+        var answertrue = document.createElement("p");
         var imgtrue = '<img src="images/true.png" class="truefalseimg">';
         var imgfalse = '<img src="images/false.png" class="truefalseimg">';
-        divs.classList.add('divs');
-        pq.classList.add('pquestions');
-        pt.classList.add('trueanswer');
-        pq.innerHTML = (i+1) + ". " + questionsandansvers[i].question;
-        pt.innerHTML = imgtrue + questionsandansvers[i][truearray[i]];
-        divs.append(pq, pt);
-        if(truearray[i] !== checkedarray[i]){
-            var pf = document.createElement("p");
-            pf.innerHTML = imgfalse + questionsandansvers[i][checkedarray[i]];
-            pf.classList.add('useranswer');
-            divs.append(pf);
+        endQuestionsAndAnsvers.classList.add('endQuestionsAndAnsvers');
+        endQuestion.classList.add('endquestions');
+        answertrue.classList.add('trueanswer');
+        endQuestion.innerHTML = (i+1) + ". " + questionsAndAnsvers[i].question;
+        answertrue.innerHTML = imgtrue + questionsAndAnsvers[i][questionsAndAnsvers[i].answer];
+        endQuestionsAndAnsvers.append(endQuestion, answertrue);
+        if(questionsAndAnsvers[i].answer !== checkedarray[i]){
+            var answerfalse = document.createElement("p");
+            answerfalse.innerHTML = imgfalse + questionsAndAnsvers[i][checkedarray[i]];
+            answerfalse.classList.add('useranswer');
+            endQuestionsAndAnsvers.append(answerfalse);
         }
-        result.append(divs);
+        result.append(endQuestionsAndAnsvers);
     }
     result.append(newgame);
 })
 newgame.addEventListener("click", function(){
     result.style.display = "none";
     start.style.display = "flex";
-    username.value="";
+    usernameinput.value="";
     checkedarray = [0,0,0,0,0];
     countback = 0;
     haveback = 0;
     checkedi = "";
-    for(let i = 0; i< radio.length; i++){
+    for(let i = 0; i < radio.length; i++){
         radio[i].checked = false;  
     }
     result.innerHTML = '';
